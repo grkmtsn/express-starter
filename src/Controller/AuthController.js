20e8dbeb4f1c7d12e2f167afa5e36319989b89b4
Tahsin Cem Yılmaz <tidusdragonh3art@gmail.com>
2018-01-15 20:11:36 +0300

    Repository Initialization

---
+++ src/Controller/AuthController.js
@@ -0,0 +1,10 @@
+const {Controller, Get} = require('@decorators/express')
+
+@Controller('/base')
+class AuthController {
+    @Get('/asd')
+    index(req,res) {
+        res.send('Hello World');
+    }
+}
+module.exports = AuthController
